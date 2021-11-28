71c8cb35d684f83c8defe4fbbee9284a5d2e3149
moh05hus <92690501+moh05hus@users.noreply.github.com>
2021-11-28 15:54:22 +0300

    Hello Turki, I added a message in a variable and added a button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+val msg = "السلام عليكم ورحمة الله وبركاته "
     }
 }
